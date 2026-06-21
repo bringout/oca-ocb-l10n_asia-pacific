faa0e60ce04e7ebbe88d70e1f67758033efd8fb8
Ernad Husremovic <hernad@bring.out.ba>
2026-06-21 18:13:25 +0200

    refresh from OCA/OCB upstream 16.0 (28 modules)

--- odoo-bringout-oca-ocb-l10n_in_edi_ewaybill/l10n_in_edi_ewaybill/models/account_edi_format.py
+++ odoo-bringout-oca-ocb-l10n_in_edi_ewaybill/l10n_in_edi_ewaybill/models/account_edi_format.py
@@ -95,6 +95,9 @@ class AccountEdiFormat(models.Model):
         elif move.l10n_in_mode in ("2", "3", "4"):
             if not move.l10n_in_transportation_doc_no and move.l10n_in_transportation_doc_date:
                 error_message.append(_("- Transport document number and date is required when Transportation Mode is Rail,Air or Ship"))
+        buyer_seller_partners = self._get_l10n_in_edi_saler_buyer_party(move)
+        if buyer_seller_partners['ship_to_details'].zip == buyer_seller_partners['dispatch_details'].zip and not move.l10n_in_distance:
+            error_message.append(_("- Set a valid distance when the dispatch and delivery pincodes are the same."))
         if error_message:
             error_message.insert(0, _("The following information are missing on the invoice (see eWayBill tab):"))
         goods_lines = move.invoice_line_ids.filtered(lambda line: not (line.display_type in ('line_section', 'line_note', 'rounding') or line.product_id.type == "service"))
@@ -584,17 +587,21 @@ class AccountEdiFormat(models.Model):
     @api.model
     def _l10n_in_edi_ewaybill_connect_to_server(self, company, url_path, params):
         user_token = self.env["iap.account"].get("l10n_in_edi")
+        IrConfigParam = self.env["ir.config_parameter"].sudo()
         params.update({
             "account_token": user_token.account_token,
-            "dbuuid": self.env["ir.config_parameter"].sudo().get_param("database.uuid"),
+            "dbuuid": IrConfigParam.get_param("database.uuid"),
             "username": company.sudo().l10n_in_edi_ewaybill_username,
             "gstin": company.vat,
         })
+        gsp_provider = IrConfigParam.get_param("l10n_in.gsp_provider")
+        if gsp_provider:
+            params["gsp_provider"] = gsp_provider
         if company.sudo().l10n_in_edi_production_env:
             default_endpoint = DEFAULT_IAP_ENDPOINT
         else:
             default_endpoint = DEFAULT_IAP_TEST_ENDPOINT
-        endpoint = self.env["ir.config_parameter"].sudo().get_param("l10n_in_edi_ewaybill.endpoint", default_endpoint)
+        endpoint = IrConfigParam.get_param("l10n_in_edi_ewaybill.endpoint", default_endpoint)
         url = "%s%s" % (endpoint, url_path)
         try:
             response = jsonrpc(url, params=params, timeout=70)
